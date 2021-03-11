89287c5e3ed6cc536d0df78155e47a6a050d2f55
Daniel Flores <danielflores125@gmail.com>
2021-03-11 10:04:52 -0800

    Implement chart to show bitcoin prices for a single month

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
+import {Line} from 'react-chartjs-2';
+import axios from 'axios'
 
 function CoinChart(){
-  const [stats, setStats] = useState(['Test'])
+  const [prices, setPrices] = useState([])
+  const [dates, setDates] = useState([])
+
+  const data = {
+    labels: dates,
+    datasets: [
+      {
+        label: 'BitCoin Price',
+        fill:false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: prices
+      }
+    ]
+  };
+
+  function retrieveCoinDeskScores(){
+    axios.get('scores')
+    .then(function(res){
+      setDates(Object.keys(res.data))
+      setPrices(Object.values(res.data))
+    })
+    .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    retrieveCoinDeskScores()
+  }, []);
+
   return(
     <div>
-      {stats}
+      <Line
+      data={data}
+      />
     </div>
   )
-
 }
-
 ReactDOM.render(
   <CoinChart/>,
   document.getElementById('app')
